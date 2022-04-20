51e1edb9d32efec386869c30a221eb4c81c67838
Josh Gachnang <josh@nang.io>
2022-04-20 10:44:06 -0500

    Move api/lint to their own repos (#4)

--- packages/api/dist/passport.d.ts
+++
@@ -1,9 +0,0 @@
-import { Schema } from "mongoose";
-export interface Options {
-    saltlen: number;
-    iterations: number;
-    keylen: number;
-    encoding: string;
-    digestAlgorithm: string;
-}
-export declare const passportLocalMongoose: (schema: Schema, opts?: Partial<Options>) => void;
--- packages/api/src/express.d.ts
+++
@@ -1,5 +0,0 @@
-declare namespace Express {
-  export interface Request {
-    user?: {_id: string | ObjectId; id: string; admin: boolean};
-  }
-}
