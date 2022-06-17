53ead9dfd4b8f002d6fbbdf3dc09582513e664ca
Josh Gachnang <josh@nang.io>
2022-06-17 15:23:33 -0500

    Fix stories using hooks
    We need to encapsulate the hooks in a component rather than just a
    story function, otherwise React gets mad about different numbers
    of hooks. Annoying but works.

--- packages/ui-demo/src/SelectList.stories.tsx
+++ packages/ui-demo/src/SelectList.stories.tsx
@@ -9,56 +9,68 @@ const options: SelectListOptions = [
   {label: "Third, A Really Long Option", value: "third"},
 ];
 
+const SelectLists = () => {
+  const [item, setItem] = React.useState(options[0].value);
+
+  return (
+    <StorybookContainer>
+      <SelectList
+        id="none"
+        options={options}
+        placeholder="Here's some placeholder text."
+        value={item}
+        onChange={(newItem) => setItem(newItem)}
+      />
+    </StorybookContainer>
+  );
+};
+
+const WithLabel = () => {
+  const [item, setItem] = React.useState(options[0].value);
+
+  return (
+    <StorybookContainer>
+      <SelectList
+        helperText="And some subtext"
+        id="none"
+        label="Enter a bunch of text"
+        options={options}
+        value={item}
+        onChange={(newItem) => setItem(newItem)}
+      />
+    </StorybookContainer>
+  );
+};
+
+const Disabled = () => {
+  const [item, setItem] = React.useState(options[0].value);
+
+  return (
+    <StorybookContainer>
+      <SelectList
+        disabled
+        id="none"
+        options={options}
+        placeholder="This is disabled"
+        value={item}
+        onChange={(newItem) => setItem(newItem)}
+      />
+    </StorybookContainer>
+  );
+};
+
 export const SelectListStories = {
   title: "Select List",
   component: SelectList,
   stories: {
     SelectLists() {
-      const [item, setItem] = React.useState(options[0].value);
-
-      return (
-        <StorybookContainer>
-          <SelectList
-            id="none"
-            options={options}
-            placeholder="Here's some placeholder text."
-            value={item}
-            onChange={(newItem) => setItem(newItem)}
-          />
-        </StorybookContainer>
-      );
+      return <SelectLists />;
     },
     WithLabel() {
-      const [item, setItem] = React.useState(options[0].value);
-
-      return (
-        <StorybookContainer>
-          <SelectList
-            helperText="And some subtext"
-            id="none"
-            label="Enter a bunch of text"
-            options={options}
-            value={item}
-            onChange={(newItem) => setItem(newItem)}
-          />
-        </StorybookContainer>
-      );
+      return <WithLabel />;
     },
     Disabled() {
-      const [item, setItem] = React.useState(options[0].value);
-
-      return (
-        <StorybookContainer>
-          <SelectList
-            disabled
-            id="none"
-            options={options}
-            placeholder="This is disabled"
-            value={item}
-            onChange={(newItem) => setItem(newItem)}
-          />
-        </StorybookContainer>
-      );
+      return <Disabled />;
     },
   },
 };
