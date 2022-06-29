48dda0be42bb7cc22a9c8caba7b1b6e808f8141e
Josh Gachnang <josh@nang.io>
2022-06-29 12:06:20 -0500

    Roll back allowing numbers in SelectList
    On the web, <option> only accepts string for values, so do the same
    in our SelectList implementation. Using numbers was causing hard to
    debug issues where the wrong item would be selected.
    
    Also make SelectList look nice on web.

--- packages/ui/src/SelectList.tsx
+++ packages/ui/src/SelectList.tsx
@@ -5,7 +5,7 @@ import RNPickerSelect from "./PickerSelect";
 import {Unifier} from "./Unifier";
 
 // Use "" if you want to have an "unset" value.
-export type SelectListOptions = {label: string; value: string | number}[];
+export type SelectListOptions = {label: string; value: string}[];
 export interface SelectListProps extends FieldWithLabelsProps {
   id?: string;
   name?: string;
@@ -17,33 +17,29 @@ export interface SelectListProps extends FieldWithLabelsProps {
   placeholder?: string;
 }
 
-export class SelectList extends React.Component<SelectListProps, {}> {
-  state = {showing: false};
-
-  render() {
-    return (
-      <RNPickerSelect
-        items={this.props.options}
-        placeholder={{}}
-        style={{
-          viewContainer: {
-            flexDirection: "row",
-            justifyContent: "center",
-            alignItems: "center",
-            minHeight: 50,
-            width: "100%",
-            // Add padding so the border doesn't mess up layouts
-            paddingHorizontal: 6,
-            paddingVertical: 4,
-            borderColor: Unifier.theme.gray,
-            borderWidth: 1,
-            borderRadius: 5,
-            backgroundColor: Unifier.theme.white,
-          },
-        }}
-        value={this.props.value}
-        onValueChange={this.props.onChange}
-      />
-    );
-  }
+export function SelectList({options, value, onChange}: SelectListProps) {
+  return (
+    <RNPickerSelect
+      items={options}
+      placeholder={{}}
+      style={{
+        viewContainer: {
+          flexDirection: "row",
+          justifyContent: "center",
+          alignItems: "center",
+          minHeight: 50,
+          width: "100%",
+          // Add padding so the border doesn't mess up layouts
+          paddingHorizontal: 6,
+          paddingVertical: 4,
+          borderColor: Unifier.theme.gray,
+          borderWidth: 1,
+          borderRadius: 5,
+          backgroundColor: Unifier.theme.white,
+        },
+      }}
+      value={value}
+      onValueChange={onChange}
+    />
+  );
 }
