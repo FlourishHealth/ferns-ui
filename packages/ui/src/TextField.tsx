fea5058e58f74c968bcd036f4017d535b54d2aba
Josh Gachnang <josh@nang.io>
2022-07-31 14:11:10 -0500

    Convert TextField to functional component

--- packages/ui/src/TextField.tsx
+++ packages/ui/src/TextField.tsx
@@ -1,5 +1,5 @@
 import moment from "moment-timezone";
-import React from "react";
+import React, {ReactElement, useState} from "react";
 import {ActivityIndicator, KeyboardTypeOptions, Platform, TextInput, View} from "react-native";
 
 import {Box} from "./Box";
@@ -12,65 +12,82 @@ import {NumberPickerActionSheet} from "./NumberPickerActionSheet";
 import {Unifier} from "./Unifier";
 import {WithLabel} from "./WithLabel";
 
-interface TextFieldState {
-  focused: boolean;
-  height: number;
-}
+const keyboardMap = {
+  date: "default",
+  email: "email-address",
+  number: "number-pad",
+  numberRange: "number-pad",
+  decimalRange: "decimal-pad",
+  decimal: "decimal-pad",
+  height: "default",
+  password: "default",
+  search: "default",
+  text: "default",
+  url: Platform.select({
+    ios: "url",
+    android: "default",
+  }),
+  username: "default",
+};
 
-export class TextField extends React.Component<TextFieldProps, TextFieldState> {
-  dateActionSheetRef: React.RefObject<any> = React.createRef();
+const textContentMap = {
+  date: "none",
+  email: "emailAddress",
+  number: "none",
+  decimal: "none",
+  decimalRange: "none",
+  height: "none",
+  password: "password",
+  search: "none",
+  text: "none",
+  url: Platform.select({
+    ios: "URL",
+    android: "none",
+  }),
+  username: "username",
+};
 
-  numberRangeActionSheetRef: React.RefObject<any> = React.createRef();
+export function TextField({
+  value,
+  height: propsHeight,
+  onChange,
+  paddingX,
+  paddingY,
+  min,
+  max,
+  type = "text",
+  searching,
+  autoComplete,
+  autoFocus,
+  disabled,
+  errorMessage,
+  errorMessageColor,
+  inputRef,
+  multiline,
+  rows,
+  placeholder,
+  grow,
+  label,
+  labelColor,
+  returnKeyType,
+  onBlur,
+  style,
+  onEnter,
+  onSubmitEditing,
+}: TextFieldProps): ReactElement {
+  const dateActionSheetRef: React.RefObject<any> = React.createRef();
+  const numberRangeActionSheetRef: React.RefObject<any> = React.createRef();
+  const decimalRangeActionSheetRef: React.RefObject<any> = React.createRef();
+  const weightActionSheetRef: React.RefObject<any> = React.createRef();
 
-  decimalRangeActionSheetRef: React.RefObject<any> = React.createRef();
+  const [focused, setFocused] = useState(false);
+  const [height, setHeight] = useState(propsHeight || 40);
 
-  weightActionSheetRef: React.RefObject<any> = React.createRef();
-
-  constructor(props: TextFieldProps) {
-    super(props);
-    this.state = {focused: false, height: props.height || 40};
-  }
-
-  keyboardMap = {
-    date: "default",
-    email: "email-address",
-    number: "number-pad",
-    numberRange: "number-pad",
-    decimalRange: "decimal-pad",
-    decimal: "decimal-pad",
-    height: "default",
-    password: "default",
-    search: "default",
-    text: "default",
-    url: Platform.select({
-      ios: "url",
-      android: "default",
-    }),
-    username: "default",
-  };
-
-  textContentMap = {
-    date: "none",
-    email: "emailAddress",
-    number: "none",
-    decimal: "none",
-    decimalRange: "none",
-    height: "none",
-    password: "password",
-    search: "none",
-    text: "none",
-    url: Platform.select({
-      ios: "URL",
-      android: "none",
-    }),
-    username: "username",
-  };
-
-  renderIcon() {
-    if (this.props.type !== "search") {
+  const renderIcon = () => {
+    if (type !== "search") {
       return null;
     }
-    if (this.props.searching === true) {
+    if (searching === true) {
       return (
         <Box marginRight={4}>
           <ActivityIndicator color={Unifier.theme.primary} size="small" />
@@ -83,216 +100,193 @@ export class TextField extends React.Component<TextFieldProps, TextFieldState> {
         </Box>
       );
     }
-  }
-
-  getBorderColor = () => {
-    if (this.props.errorMessage) {
-      return Unifier.theme.red;
-    } else if (this.state.focused) {
-      return Unifier.theme.blue;
-    } else {
-      return Unifier.theme.gray;
-    }
   };
 
-  getHeight = () => {
-    if (this.props.grow) {
-      return Math.max(40, this.state.height);
-    } else if (this.props.multiline) {
-      return this.props.height || "100%";
+  let borderColor;
+  if (errorMessage) {
+    borderColor = Unifier.theme.red;
+  } else if (focused) {
+    borderColor = Unifier.theme.blue;
+  } else {
+    borderColor = Unifier.theme.gray;
+  }
+
+  const getHeight = () => {
+    if (grow) {
+      return Math.max(40, height);
+    } else if (multiline) {
+      return height || "100%";
     } else {
       return 40;
     }
   };
+  const isHandledByModal =
+    type === "date" || type === "numberRange" || type === "decimalRange" || type === "height";
 
-  focus() {
-    // Empty
-  }
+  const isEditable = !disabled && !isHandledByModal;
 
-  isEditable() {
-    return !this.props.disabled && !this.isHandledByModal();
-  }
+  const shouldAutocorrect = type === "text" && (!autoComplete || autoComplete === "on");
 
-  isHandledByModal = () => {
-    return (
-      this.props.type === "date" ||
-      this.props.type === "numberRange" ||
-      this.props.type === "decimalRange" ||
-      this.props.type === "height"
-    );
+  const keyboardType = keyboardMap[type];
+  const textContentType = textContentMap[type || "text"];
+
+  const withLabelProps = {
+    label,
+    labelColor,
   };
-
-  shouldAutocorrect = (): boolean => {
-    return (
-      this.props.type === "text" && (!this.props.autoComplete || this.props.autoComplete === "on")
-    );
-  };
-
-  render() {
-    const type = this.props.type || "text";
-    const keyboardType = this.keyboardMap[type];
-    // let textContentType = this.textContentMap[this.props.type || "text"];
-    const {errorMessage, ...props} = this.props;
-    let value = this.props.value;
-    if (this.props.type === "date") {
-      value = moment(this.props.value).format("MM/DD/YYYY");
-    } else if (this.props.type === "height") {
-      value = `${Math.floor(Number(this.props.value) / 12)} ft, ${
-        Number(this.props.value) % 12
-      } in`;
-    }
-    return (
-      <>
-        <WithLabel
-          label={errorMessage}
-          labelColor={this.props.errorMessageColor || "red"}
-          labelPlacement="after"
-          labelSize="sm"
-        >
-          <WithLabel {...props}>
-            <View
-              style={{
-                flexDirection: "row",
-                justifyContent: "center",
-                alignItems: "center",
-                // height: 40,
-                // minHeight: this.getHeight(),
-                minHeight: this.getHeight(),
-                width: "100%",
-                // Add padding so the border doesn't mess up layouts
-                paddingHorizontal: this.props.paddingX || this.state.focused ? 10 : 14,
-                paddingVertical: this.props.paddingY || this.state.focused ? 0 : 4,
-                borderColor: this.getBorderColor(),
-                borderWidth: this.state.focused && !this.props.errorMessage ? 5 : 1,
-                borderRadius: 16,
-                backgroundColor: this.props.disabled ? Unifier.theme.gray : Unifier.theme.white,
-                overflow: "hidden",
-              }}
-              onTouchEnd={() => {
-                if (this.props.type === "date") {
-                  this.dateActionSheetRef?.current?.setModalVisible(true);
-                } else if (this.props.type === "numberRange") {
-                  this.numberRangeActionSheetRef?.current?.setModalVisible(true);
-                } else if (this.props.type === "decimalRange") {
-                  this.decimalRangeActionSheetRef?.current?.setModalVisible(true);
-                } else if (this.props.type === "height") {
-                  this.weightActionSheetRef?.current?.setModalVisible(true);
-                }
-              }}
-            >
-              {this.renderIcon()}
-              <TextInput
-                ref={(ref) => {
-                  if (this.props.inputRef) {
-                    this.props.inputRef(ref);
-                  }
-                }}
-                autoCapitalize={type === "text" ? "sentences" : "none"}
-                autoCorrect={this.shouldAutocorrect()}
-                autoFocus={this.props.autoFocus}
-                blurOnSubmit
-                // TODO: uncomment with upgrade to React 0.56.
-                // textContentType={textContentType}
-                editable={this.isEditable()}
-                keyboardType={keyboardType as KeyboardTypeOptions}
-                multiline={this.props.multiline}
-                numberOfLines={this.props.rows || 4}
-                placeholder={this.props.placeholder}
-                placeholderTextColor={Unifier.theme.gray}
-                returnKeyType={
-                  type === "number" || type === "decimal" ? "done" : this.props.returnKeyType
-                }
-                secureTextEntry={type === "password"}
-                // For react-native-autofocus
-                style={{
-                  flex: 1,
-                  paddingTop: 10,
-                  paddingRight: 10,
-                  paddingBottom: 10,
-                  paddingLeft: 0,
-                  height: this.getHeight(),
-                  width: "100%",
-                  color: Unifier.theme.darkGray,
-                  fontFamily: Unifier.theme.primaryFont,
-                  // Remove border in web.
-                  outlineWidth: 0,
-                  ...this.props.style,
-                }}
-                underlineColorAndroid="transparent"
-                value={value}
-                onBlur={() => {
-                  if (!this.isHandledByModal()) {
-                    this.setState({focused: false});
-                  }
-                  if (this.props.onBlur && this.props.value) {
-                    this.props.onBlur({value: this.props.value});
-                  }
-                  // if (this.props.type === "date") {
-                  //   this.actionSheetRef?.current?.setModalVisible(false);
-                  // }
-                }}
-                onChangeText={(text) => {
-                  this.props.onChange({value: text});
-                }}
-                onContentSizeChange={(event) => {
-                  if (!this.props.grow) {
-                    return;
-                  }
-                  this.setState({height: event.nativeEvent.contentSize.height});
-                }}
-                onFocus={() => {
-                  if (!this.isHandledByModal()) {
-                    this.setState({focused: true});
-                  }
-                }}
-                onSubmitEditing={() => {
-                  if (this.props.onEnter) {
-                    this.props.onEnter();
-                  }
-                  if (this.props.onSubmitEditing) {
-                    this.props.onSubmitEditing();
-                  }
-                }}
-              />
-            </View>
-          </WithLabel>
-        </WithLabel>
-        {this.props.type === "date" && (
-          <DateTimeActionSheet
-            actionSheetRef={this.dateActionSheetRef}
-            mode="date"
-            value={this.props.value}
-            onChange={(result) => this.props.onChange(result)}
-          />
-        )}
-        {this.props.type === "numberRange" && this.props.value && (
-          <NumberPickerActionSheet
-            actionSheetRef={this.numberRangeActionSheetRef}
-            max={this.props.max || (this.props.min || 0) + 100}
-            min={this.props.min || 0}
-            value={this.props.value}
-            onChange={(result) => this.props.onChange(result)}
-          />
-        )}
-        {this.props.type === "decimalRange" && this.props.value && (
-          <DecimalRangeActionSheet
-            actionSheetRef={this.decimalRangeActionSheetRef}
-            max={this.props.max || (this.props.min || 0) + 100}
-            min={this.props.min || 0}
-            value={this.props.value}
-            onChange={(result) => this.props.onChange(result)}
-          />
-        )}
-        {this.props.type === "height" && (
-          <HeightActionSheet
-            actionSheetRef={this.weightActionSheetRef}
-            value={this.props.value}
-            onChange={(result) => {
-              this.props.onChange(result);
+  if (type === "date") {
+    value = moment(value).format("MM/DD/YYYY");
+  } else if (type === "height") {
+    value = `${Math.floor(Number(value) / 12)} ft, ${Number(value) % 12} in`;
+  }
+  return (
+    <>
+      <WithLabel
+        label={errorMessage}
+        labelColor={errorMessageColor || "red"}
+        labelPlacement="after"
+        labelSize="sm"
+      >
+        <WithLabel {...withLabelProps}>
+          <View
+            style={{
+              flexDirection: "row",
+              justifyContent: "center",
+              alignItems: "center",
+              // height: 40,
+              // minHeight: getHeight(),
+              minHeight: getHeight(),
+              width: "100%",
+              // Add padding so the border doesn't mess up layouts
+              paddingHorizontal: paddingX || focused ? 10 : 14,
+              paddingVertical: paddingY || focused ? 0 : 4,
+              borderColor,
+              borderWidth: focused && !errorMessage ? 5 : 1,
+              borderRadius: 16,
+              backgroundColor: disabled ? Unifier.theme.gray : Unifier.theme.white,
+              overflow: "hidden",
             }}
-          />
-        )}
-      </>
-    );
-  }
+            onTouchEnd={() => {
+              if (type === "date") {
+                dateActionSheetRef?.current?.setModalVisible(true);
+              } else if (type === "numberRange") {
+                numberRangeActionSheetRef?.current?.setModalVisible(true);
+              } else if (type === "decimalRange") {
+                decimalRangeActionSheetRef?.current?.setModalVisible(true);
+              } else if (type === "height") {
+                weightActionSheetRef?.current?.setModalVisible(true);
+              }
+            }}
+          >
+            {renderIcon()}
+            <TextInput
+              ref={(ref) => {
+                if (inputRef) {
+                  inputRef(ref);
+                }
+              }}
+              autoCapitalize={type === "text" ? "sentences" : "none"}
+              autoCorrect={shouldAutocorrect}
+              autoFocus={autoFocus}
+              blurOnSubmit
+              editable={isEditable}
+              keyboardType={keyboardType as KeyboardTypeOptions}
+              multiline={multiline}
+              numberOfLines={rows || 4}
+              placeholder={placeholder}
+              placeholderTextColor={Unifier.theme.gray}
+              returnKeyType={type === "number" || type === "decimal" ? "done" : returnKeyType}
+              secureTextEntry={type === "password"}
+              style={{
+                flex: 1,
+                paddingTop: 10,
+                paddingRight: 10,
+                paddingBottom: 10,
+                paddingLeft: 0,
+                height: getHeight(),
+                width: "100%",
+                color: Unifier.theme.darkGray,
+                fontFamily: Unifier.theme.primaryFont,
+                // Remove border in web.
+                outlineWidth: 0,
+                ...style,
+              }}
+              // For react-native-autofocus
+              textContentType={textContentType}
+              underlineColorAndroid="transparent"
+              value={value}
+              onBlur={() => {
+                if (!isHandledByModal) {
+                  setFocused(false);
+                }
+                if (onBlur && value) {
+                  onBlur({value});
+                }
+                // if (type === "date") {
+                //   actionSheetRef?.current?.setModalVisible(false);
+                // }
+              }}
+              onChangeText={(text) => {
+                onChange({value: text});
+              }}
+              onContentSizeChange={(event) => {
+                if (!grow) {
+                  return;
+                }
+                setHeight(event.nativeEvent.contentSize.height);
+              }}
+              onFocus={() => {
+                if (!isHandledByModal) {
+                  setFocused(true);
+                }
+              }}
+              onSubmitEditing={() => {
+                if (onEnter) {
+                  onEnter();
+                }
+                if (onSubmitEditing) {
+                  onSubmitEditing();
+                }
+              }}
+            />
+          </View>
+        </WithLabel>
+      </WithLabel>
+      {type === "date" && Platform.OS !== "web" && (
+        <DateTimeActionSheet
+          actionSheetRef={dateActionSheetRef}
+          mode="date"
+          value={value}
+          onChange={(result) => onChange(result)}
+        />
+      )}
+      {type === "numberRange" && value && (
+        <NumberPickerActionSheet
+          actionSheetRef={numberRangeActionSheetRef}
+          max={max || (min || 0) + 100}
+          min={min || 0}
+          value={value}
+          onChange={(result) => onChange(result)}
+        />
+      )}
+      {type === "decimalRange" && value && (
+        <DecimalRangeActionSheet
+          actionSheetRef={decimalRangeActionSheetRef}
+          max={max || (min || 0) + 100}
+          min={min || 0}
+          value={value}
+          onChange={(result) => onChange(result)}
+        />
+      )}
+      {type === "height" && (
+        <HeightActionSheet
+          actionSheetRef={weightActionSheetRef}
+          value={value}
+          onChange={(result) => {
+            onChange(result);
+          }}
+        />
+      )}
+    </>
+  );
 }
