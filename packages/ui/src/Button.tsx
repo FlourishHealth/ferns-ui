346b993fe9bf4b01aaf7585edb4057bb7f60e885
Abbie Coghlan <abbie.coghlan@gmail.com>
2022-10-18 14:12:04 -0500

    Add confirmation modal and associated props to Button.tsx

--- packages/ui/src/Button.tsx
+++ packages/ui/src/Button.tsx
@@ -5,6 +5,7 @@ import {ActivityIndicator, TouchableOpacity} from "react-native";
 import {Box} from "./Box";
 import {ButtonColor, Color, GestaltIconName, IconPrefix, UnifiedTheme} from "./Common";
 import {Icon} from "./Icon";
+import {Modal} from "./Modal";
 import {Text} from "./Text";
 import {Unifier} from "./Unifier";
 
@@ -25,6 +26,9 @@ export interface ButtonProps {
   icon?: GestaltIconName | string;
   iconPrefix?: IconPrefix;
   iconColor?: ButtonColor | Color;
+  withConfirmation?: boolean;
+  confirmationText?: string;
+  confirmationHeading?: string;
 }
 
 const buttonTextColor: {[buttonColor: string]: "white" | "darkGray"} = {
@@ -60,8 +64,12 @@ export function Button({
   size,
   onClick,
   color = "lightGray",
+  withConfirmation = false,
+  confirmationText = "Are you sure you want to continue?",
+  confirmationHeading = "Confirm",
 }: ButtonProps) {
   const [loading, setLoading] = useState(propsLoading);
+  const [showConfirmation, setShowConfirmation] = useState(false);
 
   const getBackgroundColor = (backgroundColor: string): string => {
     if (type === "ghost" || type === "outline") {
@@ -92,71 +100,101 @@ export function Button({
   if (color === "gray") {
     color = "lightGray";
   }
+
+  const renderConfirmation = () => {
+    return (
+      <Modal
+        heading={confirmationHeading}
+        primaryButtonOnClick={() => {
+          onClick();
+          setShowConfirmation(false);
+        }}
+        primaryButtonText="Confirm"
+        secondaryButtonOnClick={(): void => setShowConfirmation(false)}
+        secondaryButtonText="Cancel"
+        size="sm"
+        visible={showConfirmation}
+        onDismiss={(): void => {
+          setShowConfirmation(false);
+        }}
+      >
+        <Text>{confirmationText}</Text>
+      </Modal>
+    );
+  };
+
   return (
-    <TouchableOpacity
-      disabled={disabled || loading}
-      style={{
-        alignSelf: inline === true ? undefined : "stretch",
-        height: HEIGHTS[size || "md"],
-        backgroundColor: getBackgroundColor(color),
-        // width: inline === true ? undefined : "100%",
-        flexShrink: inline ? 1 : 0,
-        // flexGrow: inline ? 0 : 1,
-        alignItems: "center",
-        justifyContent: "center",
-        borderRadius: 5,
-        borderColor: getBorderColor(color),
-        borderWidth: type === "outline" ? 2 : 0,
-        opacity: disabled ? 0.4 : 1,
-        flexDirection: "row",
-        paddingHorizontal: 4 * 2,
-      }}
-      onPress={debounce(
-        async () => {
-          Unifier.utils.haptic();
-          setLoading(true);
-          try {
-            if (onClick) {
-              await onClick();
+    <>
+      {Boolean(withConfirmation) && renderConfirmation()}
+      <TouchableOpacity
+        disabled={disabled || loading}
+        style={{
+          alignSelf: inline === true ? undefined : "stretch",
+          height: HEIGHTS[size || "md"],
+          backgroundColor: getBackgroundColor(color),
+          // width: inline === true ? undefined : "100%",
+          flexShrink: inline ? 1 : 0,
+          // flexGrow: inline ? 0 : 1,
+          alignItems: "center",
+          justifyContent: "center",
+          borderRadius: 5,
+          borderColor: getBorderColor(color),
+          borderWidth: type === "outline" ? 2 : 0,
+          opacity: disabled ? 0.4 : 1,
+          flexDirection: "row",
+          paddingHorizontal: 4 * 2,
+        }}
+        onPress={debounce(
+          async () => {
+            Unifier.utils.haptic();
+            setLoading(true);
+            try {
+              if (withConfirmation) {
+                if (!showConfirmation) {
+                  setShowConfirmation(true);
+                }
+              } else if (onClick) {
+                await onClick();
+              }
+            } catch (e) {
+              setLoading(false);
+              throw e;
             }
-          } catch (e) {
             setLoading(false);
-            throw e;
-          }
-          setLoading(false);
-        },
-        500,
-        {leading: true}
-      )}
-    >
-      {icon !== undefined && (
-        <Box paddingX={2}>
-          <Icon
+          },
+          500,
+          {leading: true}
+        )}
+      >
+        {icon !== undefined && (
+          <Box paddingX={2}>
+            <Icon
+              color={getTextColor(color as Color)}
+              name={icon}
+              prefix={iconPrefix || "far"}
+              size={size}
+            />
+          </Box>
+        )}
+        {Boolean(children) && children}
+        {Boolean(text) && (
+          <Text
             color={getTextColor(color as Color)}
-            name={icon}
-            prefix={iconPrefix || "far"}
+            font="button"
+            inline={inline}
             size={size}
-          />
-        </Box>
-      )}
-      {Boolean(children) && children}
-      {Boolean(text) && (
-        <Text
-          color={getTextColor(color as Color)}
-          font="button"
-          inline={inline}
-          size={size}
-          skipLinking
-          weight="bold"
-        >
-          {text}
-        </Text>
-      )}
-      {(loading || loading) && (
-        <Box marginLeft={2}>
-          <ActivityIndicator color={getTextColor(color as Color)} size="small" />
-        </Box>
-      )}
-    </TouchableOpacity>
+            skipLinking
+            weight="bold"
+          >
+            {text}
+          </Text>
+        )}
+        {(loading || loading) && (
+          <Box marginLeft={2}>
+            <ActivityIndicator color={getTextColor(color as Color)} size="small" />
+          </Box>
+        )}
+      </TouchableOpacity>
+    </>
   );
 }
