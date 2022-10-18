7b3db922191eafbf77477006e618385ce78cf522
Abbie Coghlan <abbie.coghlan@gmail.com>
2022-10-18 13:18:15 -0500

    Fix onClick in Button.stories.tsx

--- packages/ui-demo/src/Button.stories.tsx
+++ packages/ui-demo/src/Button.stories.tsx
@@ -8,28 +8,28 @@ const allColorButtons = (props: Partial<ButtonProps>) => {
   return (
     <StorybookContainer>
       <Box paddingY={1}>
-        <Button text="Default" onClick={action("clicked")} {...props} />
+        <Button text="Default" onClick={() => action("clicked")} {...props} />
       </Box>
       <Box paddingY={1}>
-        <Button color="primary" text="Primary" onClick={action("clicked")} {...props} />
+        <Button color="primary" text="Primary" onClick={() => action("clicked")} {...props} />
       </Box>
       <Box paddingY={1}>
-        <Button color="secondary" text="Secondary" onClick={action("clicked")} {...props} />
+        <Button color="secondary" text="Secondary" onClick={() => action("clicked")} {...props} />
       </Box>
       <Box paddingY={1}>
-        <Button color="tertiary" text="Tertiary" onClick={action("clicked")} {...props} />
+        <Button color="tertiary" text="Tertiary" onClick={() => action("clicked")} {...props} />
       </Box>
       <Box paddingY={1}>
-        <Button color="accent" text="Accent" onClick={action("clicked")} {...props} />
+        <Button color="accent" text="Accent" onClick={() => action("clicked")} {...props} />
       </Box>
       <Box paddingY={1}>
-        <Button color="red" text="Red" onClick={action("clicked")} {...props} />
+        <Button color="red" text="Red" onClick={() => action("clicked")} {...props} />
       </Box>
       <Box paddingY={1}>
-        <Button color="darkGray" text="Dark Gray" onClick={action("clicked")} {...props} />
+        <Button color="darkGray" text="Dark Gray" onClick={() => action("clicked")} {...props} />
       </Box>
       <Box paddingY={1}>
-        <Button color="gray" text="Gray" onClick={action("clicked")} {...props} />
+        <Button color="gray" text="Gray" onClick={() => action("clicked")} {...props} />
       </Box>
       <Box paddingY={1}>
         <Button
