050896b8151b0e74b81ff11bf673dcb6e19fc46d
Kyle Kurtz <kyle@kurtz.io>
2022-10-28 13:06:27 -0500

    Separate out and fix CustomSelect

---
+++ packages/ui/src/CustomSelect.tsx
@@ -0,0 +1,77 @@
+import React, {ReactElement, useEffect, useState} from "react";
+
+import {Box} from "./Box";
+import {SelectList} from "./SelectList";
+import {TextField} from "./TextField";
+
+export interface CustomSelectProps {
+  value: string;
+  onChange: (value: string) => void;
+  options: Array<{label: string; value: string}>;
+  placeholder?: string;
+  disabled?: boolean;
+  label?: string;
+  labelColor?: string;
+}
+
+export const CustomSelect = ({
+  value,
+  onChange,
+  placeholder,
+  disabled,
+  options,
+}: CustomSelectProps): ReactElement | null => {
+  const [customValue, setCustomValue] = useState("");
+  const [showCustomInput, setShowCustomInput] = useState(false);
+
+  useEffect(() => {
+    if (!showCustomInput) {
+      setCustomValue(value);
+    }
+  }, [showCustomInput, value]);
+
+  // Custom select has 3 values - the overall field value, the value of the select menu, and the value of the custom input
+  const handleCustomSelectListChange = (newValue: string) => {
+    // If "custom" is selected from the dropdown, toggle the custom input open and clear the previous value
+    if (newValue === "custom") {
+      setShowCustomInput(true);
+      setCustomValue("custom");
+      onChange("");
+    }
+
+    // If any non-custom value is selected
+    else {
+      // Close the custom input if open and clear the value
+      if (showCustomInput) {
+        setShowCustomInput(false);
+      }
+
+      // Update the field value and select value
+      onChange(newValue);
+    }
+  };
+
+  return (
+    <>
+      <SelectList
+        id="providedOptions"
+        options={[...options, {label: "Custom", value: "custom"}]}
+        placeholder="Select an option"
+        value={customValue}
+        onChange={handleCustomSelectListChange}
+      />
+      {Boolean(showCustomInput) && (
+        <Box paddingY={2}>
+          <TextField
+            disabled={disabled}
+            id="customOptions"
+            placeholder={placeholder}
+            type="text"
+            value={value}
+            onChange={(result) => onChange(result.value)}
+          />
+        </Box>
+      )}
+    </>
+  );
+};
