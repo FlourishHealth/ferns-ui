1f6488929876e908677892d5fa160f0368fdbd1e
Josh Gachnang <josh@nang.io>
2023-03-08 15:09:09 -0600

    Add Host to FernsProvider
    Required for toasts

--- packages/ui/src/FernsProvider.tsx
+++ packages/ui/src/FernsProvider.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import {Host} from "react-native-portalize";
 import {ToastProvider} from "react-native-toast-notifications";
 
 import {Toast} from "./Toast";
@@ -14,7 +15,7 @@ export function FernsProvider({children}: {children: React.ReactNode}): React.Re
       renderToast={(toastOptions) => <Toast {...(toastOptions as any)} />}
       swipeEnabled
     >
-      {children}
+      <Host>{children}</Host>
     </ToastProvider>
   );
 }
