33889dd0106071dfd1c0dbcaba89e496852d559e
Josh Gachnang <josh@nang.io>
2023-09-26 07:32:05 -0500

    Update peer deps

--- packages/ui/updateDeps.js
+++ packages/ui/updateDeps.js
@@ -1,3 +1,7 @@
+// Updates all the peerDependencies to match the devDependencies
+// This is useful when you want to update all the dependencies to the latest version
+// and you don't want to have to manually update the peerDependencies
+
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const pkg = require("./package.json");
 if (require.main === module) {
