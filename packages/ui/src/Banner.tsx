bd7ce463af3e0edeb3a142336c26bd1f607b64b3
Josh Gachnang <josh@nang.io>
2023-10-11 16:12:46 -0500

    Update Banner to functional, add permanent banners

--- packages/ui/src/Banner.tsx
+++ packages/ui/src/Banner.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 
 import {Box} from "./Box";
 import {BoxColor, ButtonColor, Rounding, TextColor} from "./Common";
@@ -6,119 +6,136 @@ import {IconButton} from "./IconButton";
 import {Text} from "./Text";
 import {Unifier} from "./Unifier";
 
-// import {faTimesCircle, faArrowRight} from "@fortawesome/free-solid-svg-icons";
-
 export interface BannerProps {
   id: string;
+  dismissible?: boolean;
   text: string;
   subtext?: string;
   color?: BoxColor;
   textColor?: TextColor;
   negativeXMargin?: number;
-  bold?: boolean;
   shape?: Rounding;
-  type?: "dismiss" | "action";
+  type?: "dismiss" | "action" | "permanent";
   onClick?: () => void;
 }
 
-// library.add(faTimesCircle);
-// library.add(faArrowRight);
-
-interface BannerState {
-  show: boolean;
+function getKey(id: string): string {
+  return `@FernsUI:${id}`;
 }
 
-function getKey(id: string) {
-  return `@ReactUnifier:${id}`;
-}
-
-export const hideBanner = (id: string) => {
+export const hideBanner = (id: string): Promise<void> => {
   console.debug(`[banner] Hiding ${getKey(id)} `);
-  Unifier.storage.setItem(getKey(id), "true");
+  return Unifier.storage.setItem(getKey(id), "true");
 };
 
-export class Banner extends React.Component<BannerProps, BannerState> {
-  state = {show: false};
+export const Banner = ({
+  id,
+  text,
+  subtext,
+  color,
+  textColor,
+  negativeXMargin,
+  shape,
+  type = "dismiss",
+  onClick,
+}: BannerProps): React.ReactElement | null => {
+  // If the banner is permanent, show it immediately.
+  const [show, setShow] = useState(type === "permanent");
 
-  async componentDidMount() {
-    const seen = await Unifier.storage.getItem(getKey(this.props.id));
-    console.debug(`[banner] ${getKey(this.props.id)} seen? ${seen}`);
-    this.setState({show: !seen});
-  }
+  // Load seen from async storage.
+  useEffect(() => {
+    // Always show permanent banners.
+    if (type === "permanent") {
+      return;
+    }
 
-  dismiss = () => {
-    hideBanner(this.props.id);
-    this.setState({show: false});
+    Unifier.storage.getItem(getKey(id)).then((isSeen) => {
+      console.debug(`[banner] ${getKey(id)} seen? ${isSeen}`);
+      setShow(!isSeen);
+    });
+  }, [id, type]);
+
+  const dismiss = (): void => {
+    if (type === "permanent") {
+      return;
+    }
+    hideBanner(id);
+    setShow(false);
   };
 
-  render() {
-    if (!this.state.show) {
+  const renderButton = (): React.ReactElement | null => {
+    if (type === "permanent") {
       return null;
     }
-    const type = this.props.type || "dismiss";
-
-    if (type === "action" && !this.props.onClick) {
-      console.warn("Banners with type action require an onClick property.");
-    }
-    const negativeMargin = (this.props.negativeXMargin || 0) * -4;
-
     return (
-      <Box
-        color={this.props.color || "secondaryDark"}
-        dangerouslySetInlineStyle={{
-          __style: {
-            marginLeft: negativeMargin,
-            marginRight: negativeMargin,
-          },
-        }}
-        direction="row"
-        justifyContent="between"
-        marginBottom={3}
-        marginTop={3}
-        paddingX={3}
-        paddingY={2}
-        rounding={this.props.shape}
-        shadow
-        width={Unifier.utils.dimensions().width || "100%"}
-        onClick={this.dismiss}
-      >
-        <Box alignItems="center" direction="column" flex="shrink" justifyContent="center">
-          <Box paddingY={1}>
-            <Text align="center" color={this.props.textColor || "white"} weight="bold">
-              {this.props.text}
-            </Text>
-          </Box>
-          {this.props.subtext && (
-            <Box paddingY={1}>
-              <Text align="center" color={this.props.textColor || "white"}>
-                {this.props.subtext}
-              </Text>
-            </Box>
-          )}
-        </Box>
-        <Box alignItems="center" display="block" justifyContent="center" width={40}>
-          {type === "dismiss" && (
-            <IconButton
-              accessibilityLabel=""
-              icon="times-circle"
-              // size="lg"
-              iconColor={(this.props.textColor || "white") as ButtonColor}
-              prefix="fas"
-              onClick={() => this.dismiss()}
-            />
-          )}
-          {type === "action" && (
-            <IconButton
-              accessibilityLabel=""
-              icon="arrow-right"
-              // size="lg"
-              iconColor={(this.props.textColor || "white") as ButtonColor}
-              prefix="fas"
-              onClick={() => this.props.onClick && this.props.onClick()}
-            />
-          )}
-        </Box>
+      <Box alignItems="center" display="block" justifyContent="center" width={40}>
+        {type === "dismiss" && (
+          <IconButton
+            accessibilityLabel=""
+            icon="times-circle"
+            iconColor={(textColor || "white") as ButtonColor}
+            prefix="fas"
+            onClick={dismiss}
+          />
+        )}
+        {type === "action" && (
+          <IconButton
+            accessibilityLabel=""
+            icon="arrow-right"
+            iconColor={(textColor || "white") as ButtonColor}
+            prefix="fas"
+            onClick={(): void => onClick?.()}
+          />
+        )}
       </Box>
     );
+  };
+
+  if (!show) {
+    return null;
   }
-}
+
+  if (type === "action" && !onClick) {
+    console.warn("Banners with type action require an onClick property.");
+  }
+
+  const negativeMargin = (negativeXMargin || 0) * -4;
+
+  return (
+    <Box
+      color={color || "secondaryDark"}
+      dangerouslySetInlineStyle={{
+        __style: {
+          marginLeft: negativeMargin,
+          marginRight: negativeMargin,
+        },
+      }}
+      direction="row"
+      justifyContent="between"
+      marginBottom={3}
+      marginTop={3}
+      paddingX={3}
+      paddingY={2}
+      rounding={shape}
+      shadow
+      width={Unifier.utils.dimensions().width || "100%"}
+      onClick={type === "permanent" ? undefined : dismiss}
+    >
+      <Box alignItems="center" direction="column" flex="shrink" justifyContent="center">
+        <Box paddingY={1}>
+          <Text align="center" color={textColor || "white"} weight="bold">
+            {text}
+          </Text>
+        </Box>
+        {subtext && (
+          <Box paddingY={1}>
+            <Text align="center" color={textColor || "white"}>
+              {subtext}
+            </Text>
+          </Box>
+        )}
+      </Box>
+      {renderButton()}
+    </Box>
+  );
+};
