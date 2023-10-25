b7b172a663f2d3de7f1dabb5c5134241e1d7fea6
Abbie Coghlan <abbie.coghlan@gmail.com>
2023-10-25 10:05:34 -0500

    wrap ToastProvider with ThemeProvider to allow access to themeContext in toast componenet

--- packages/ui/src/FernsProvider.tsx
+++ packages/ui/src/FernsProvider.tsx
@@ -7,18 +7,18 @@ import {Toast} from "./Toast";
 
 export function FernsProvider({children}: {children: React.ReactNode}): React.ReactElement {
   return (
-    <ToastProvider
-      animationDuration={250}
-      animationType="slide-in"
-      duration={50000}
-      offset={50}
-      placement="bottom"
-      renderToast={(toastOptions) => <Toast {...(toastOptions as any)} />}
-      swipeEnabled
-    >
-      <ThemeProvider>
+    <ThemeProvider>
+      <ToastProvider
+        animationDuration={250}
+        animationType="slide-in"
+        duration={50000}
+        offset={50}
+        placement="bottom"
+        renderToast={(toastOptions) => <Toast {...(toastOptions as any)} />}
+        swipeEnabled
+      >
         <Host>{children}</Host>
-      </ThemeProvider>
-    </ToastProvider>
+      </ToastProvider>
+    </ThemeProvider>
   );
 }
