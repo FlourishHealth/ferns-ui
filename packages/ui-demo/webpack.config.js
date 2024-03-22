439f6c9b7d142dfc02115c089f1af64be041c273
Josh Gachnang <josh@nang.io>
2024-03-22 11:41:30 -0500

    Remove metro config and yarn workspaces to fix build
    Not sure why this suddenly is failing on netlify. But yarn workspaces is
    now handled automatically by the default metro.

--- packages/ui-demo/webpack.config.js
+++ packages/ui-demo/webpack.config.js
@@ -1,9 +1,23 @@
-// https://github.com/expo/expo/blob/master/packages/expo-yarn-workspaces/README.md
-
-const {createWebpackConfigAsync} = require("expo-yarn-workspaces/webpack");
+const createExpoWebpackConfigAsync = require("@expo/webpack-config");
+const {resolve} = require("path");
 
 module.exports = async function (env, argv) {
-  const config = await createWebpackConfigAsync(env, argv);
+  const config = await createExpoWebpackConfigAsync(env, argv);
+
+  // resolve victory-native as victory for the Web app
+  config.resolve.alias["victory-native"] = "victory";
+
+  config.resolve.fallback = {
+    crypto: require.resolve("crypto-browserify"),
+    stream: require.resolve("stream-browserify"),
+  };
+
+  config.module.rules.push({
+    test: /.m?js/,
+    resolve: {
+      fullySpecified: false,
+    },
+  });
 
   return config;
 };
