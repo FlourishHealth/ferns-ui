e89ffcf2ece3728578a48630111045e5b0e799af
Freddie Cuenca <freddie@flourish.health>
2024-05-29 10:33:38 -0600

    Add styles to handle scroll and overflow in web

--- packages/ui/src/SideDrawer.tsx
+++ packages/ui/src/SideDrawer.tsx
@@ -2,13 +2,12 @@ import React, {ReactElement} from "react";
 import {Platform, SafeAreaView, StyleProp, ViewStyle} from "react-native";
 import {Drawer} from "react-native-drawer-layout";
 
-import {Box} from "./Box";
 import {SideDrawerProps} from "./Common";
 
 const DEFAULT_STYLES: StyleProp<ViewStyle> = {
   width: Platform.OS === "web" ? "40%" : "95%",
   height: "100%",
-  backgroundColor: "lightgray",
+  backgroundColor: "green",
   borderWidth: 1,
   borderColor: "gray",
 };
@@ -26,19 +25,26 @@ export const SideDrawer = ({
   const renderDrawerContent = (): ReactElement => {
     return <SafeAreaView>{renderContent()}</SafeAreaView>;
   };
+
+  const addWebScroll = (): any => {
+    if (Platform.OS === "web") {
+      return {display: isOpen ? "flex" : "none", overflow: "scroll"};
+    } else {
+      return {};
+    }
+  };
+
   return (
-    <Box height="100%" overflow="hidden" width="100%">
-      <Drawer
-        drawerPosition={position}
-        drawerStyle={[DEFAULT_STYLES, drawerStyles]}
-        drawerType={drawerType}
-        open={isOpen}
-        renderDrawerContent={renderDrawerContent}
-        onClose={onClose}
-        onOpen={onOpen}
-      >
-        {children}
-      </Drawer>
-    </Box>
+    <Drawer
+      drawerPosition={position}
+      drawerStyle={[DEFAULT_STYLES, drawerStyles, addWebScroll()]}
+      drawerType={drawerType}
+      open={isOpen}
+      renderDrawerContent={renderDrawerContent}
+      onClose={onClose}
+      onOpen={onOpen}
+    >
+      {children}
+    </Drawer>
   );
 };
