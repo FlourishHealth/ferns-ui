82c67f6446dccfab5f7a5c4dd107eef85d06cad2
Abbie Coghlan <abbie.coghlan@gmail.com>
2024-06-11 15:06:55 -0500

    WIP - update badge

--- packages/ui/src/Badge.tsx
+++ packages/ui/src/Badge.tsx
@@ -1,66 +1,73 @@
-import React from "react";
+import React, {useContext} from "react";
 
 import {Box} from "./Box";
 import {BadgeProps} from "./Common";
 import {Icon} from "./Icon";
 import {Text} from "./Text";
+import {ThemeContext} from "./Theme";
+
+
+import { View, StyleSheet } from "react-native";
+
+
 
 export const Badge = ({
-  title,
-  position = "middle",
-  type = "info",
-  color,
-  iconProps,
-  fontWeight = "bold",
+  text,
+  status = "info",
+  secondary = false,
+  variant = "text",
 }: BadgeProps): React.ReactElement => {
-  // TODO: Badge encompasses what we used to call pill, needs to be updated to include that.
-  // It has text/icon/number variants, as well as light and bold color variants,
-  // then actual color variants ("error", "success", etc)
+  const {theme} = useContext(ThemeContext);
 
-  if (color && type !== "custom") {
-    console.warn('Badge color only supported when `type` is set to "custom".');
-  }
 
-  function renderIcon(): React.ReactElement | null {
-    if (iconProps && iconProps.name) {
-      return (
-        <Box marginRight={title ? 1 : 0}>
-          <Icon color={iconProps?.color ? iconProps.color : "inverted"} {...iconProps} size="sm" />
-        </Box>
-      );
-    } else {
-      return null;
-    }
-  }
 
-  function renderLabel(): React.ReactElement | null {
-    if (!title) {
-      return null;
-    }
-    return (
-      <Text color="inverted" size="sm" weight={fontWeight}>
-        {title}
-      </Text>
-    );
-  }
+
+  
+// use status to set the color of the badge
+if (status === "info") {
+  styles.backgroundColor = "info";
+  styles.textColor = "white";
+} else if (status === "success") {
+  styles.backgroundColor = "success";
+}
+  
 
   return (
-    <Box
-      alignItems="baseline"
-      alignSelf={position === "middle" ? "center" : position === "bottom" ? "end" : "start"}
-      color="primary"
-      direction="row"
-      height="min-content"
-      justifyContent="center"
-      marginLeft={1}
-      marginTop={position === "top" ? -1 : 0}
-      paddingX={2}
-      paddingY={1}
-      rounding="md"
-      width="max-content"
-    >
-      {renderIcon()}
-      {renderLabel()}
-    </Box>
+    // <Box
+    //   alignItems="baseline"
+    //   alignSelf={alignSelf}
+    //   color="primary"
+    //   direction="row"
+    //   height="min-content"
+    //   justifyContent="center"
+    //   marginLeft={1}
+    //   marginTop={direction === "top" ? -1 : 0}
+    //   paddingX={2}
+    //   paddingY={1}
+    //   rounding="md"
+    //   width="max-content"
+    // >
+    // </Box>
+
+    <View style={{
+      alignItems: "baseline",
+      alignSelf: alignSelf,
+    flexDirection: "row",
+    height: "min-content",
+    justifyContent: "center",
+    marginLeft: 1,
+    marginTop: direction === "top" ? -1 : 0,
+    paddingX: 2,
+    paddingY: 1,
+    rounding: "md",
+    width: "max-content",
+  }
+}>
+      <Icon name="check" size="sm" color="white" />
+      <Text color={secondary ? theme.text.primary : theme.text.inverted} fontWeight="bold" />
+    </View>
+
+
   );
 };
+
