9dc97a2cef981f17cd74706605ec9b0ed1266536
Hannah <Hannah.Chamorro@gmail.com>
2024-06-27 13:57:37 -0400

    Accordion (#603)
    * v0.47.10 (#578)
    
    Co-authored-by: GitHub CD bot <github-cd-bot@nang.io>
    
    * accordion
    
    * ope
    
    * hit slop
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: GitHub CD bot <github-cd-bot@nang.io>

--- packages/ui/src/Common.ts
+++ packages/ui/src/Common.ts
@@ -11,6 +11,50 @@ import {
 } from "./CommonIconTypes";
 import {SelectListOptions} from "./SelectList";
 
+export interface AccordionProps {
+  /**
+   * The content to be displayed inside the accordion.
+   */
+  children: React.ReactNode;
+
+  /**
+   * If true, an information modal will be included.
+   * @default false
+   */
+  includeInfoModal?: boolean;
+
+  /**
+   * The content of the information modal.
+   */
+  infoModalChildren?: ModalProps["children"];
+
+  /**
+   * The subtitle of the information modal.
+   */
+  infoModalSubTitle?: ModalProps["subTitle"];
+
+  /**
+   * The text content of the information modal.
+   */
+  infoModalText?: ModalProps["text"];
+
+  /**
+   * The title of the information modal.
+   */
+  infoModalTitle?: ModalProps["title"];
+
+  /**
+   * If true, the accordion will be collapsed.
+   * @default true
+   */
+  isCollapsed?: boolean;
+
+  /**
+   * The title of the accordion.
+   */
+  title: string;
+}
+
 export interface BaseProfile {
   email: string;
   id: string;
