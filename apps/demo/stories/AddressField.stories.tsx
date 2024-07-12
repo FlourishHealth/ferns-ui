c36a139cdb5bd9c1c57cca0685258a1b760e3da8
Freddie Cuenca <freddie@flourish.health>
2024-07-11 19:09:46 -0600

    Fix demo for address

--- apps/demo/stories/AddressField.stories.tsx
+++ apps/demo/stories/AddressField.stories.tsx
@@ -2,7 +2,17 @@ import {Box, formatAddress, Text} from "ferns-ui";
 import React from "react";
 
 export const AddressFieldDemo = ({address}: any) => {
-  const formattedAddress = formatAddress(address);
+  const formattedAddress = formatAddress(
+    address ?? {
+      address1: "1234 Main St",
+      address2: "Apt 123",
+      city: "Anytown",
+      state: "California",
+      zipcode: "12345",
+      countyName: "Any County",
+      countyCode: "67890",
+    }
+  );
   return (
     <Box alignContent="end">
       <Text align="right">{formattedAddress}</Text>
