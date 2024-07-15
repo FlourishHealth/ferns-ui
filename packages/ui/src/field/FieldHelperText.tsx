fa423b4c2255503404a895e3e40751b2156da9e1
Josh Gachnang <josh@nang.io>
2024-07-14 22:19:00 -0500

    Temp rename FieldElements

---
+++ packages/ui/src/field/FieldHelperText.tsx
@@ -0,0 +1,20 @@
+// This component is intended to be used as a helper text for form fields, specifically for text
+// fields and text areas. It is not intended to be used as a standalone component.
+import React, {FC} from "react";
+import {Text, View} from "react-native";
+
+import {useTheme} from "../Theme";
+
+interface FieldHelperTextProps {
+  text: string;
+}
+
+export const FieldHelperText: FC<FieldHelperTextProps> = ({text}) => {
+  const {theme} = useTheme();
+
+  return (
+    <View style={{marginTop: 2}}>
+      <Text style={{fontSize: 12, color: theme.text.primary, lineHeight: 16}}>{text}</Text>
+    </View>
+  );
+};
