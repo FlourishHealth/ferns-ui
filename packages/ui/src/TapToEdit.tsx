9fbba9a718159ed156644a6bf58588dc70cd63c0
Josh Gachnang <josh@nang.io>
2024-07-17 14:20:29 -0500

    Fix links in TapToEdit
    If a link was set, only the base domain would be used because Text intercepted the link and would open it.

--- packages/ui/src/TapToEdit.tsx
+++ packages/ui/src/TapToEdit.tsx
@@ -233,7 +233,7 @@ export const TapToEdit = ({
               onClick={isClickable ? openLink : undefined}
             >
               {Boolean(fieldProps?.type !== "textarea") && (
-                <Text align="right" underline={isClickable}>
+                <Text align="right" skipLinking underline={isClickable}>
                   {displayValue}
                 </Text>
               )}
