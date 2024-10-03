a19fad7a1c3f893a009954641777a0248af723db
abbiecoghlan <66394682+abbiecoghlan@users.noreply.github.com>
2024-10-03 14:56:16 -0500

    fix invalid date issue when clearing value from date/time pickers + add watch script (#742)
    * return placehold instead of invalid date when clearing value from date/time pickers
    
    * add watch script in root
    
    * update readme
    
    * return empty string in formatValue if val is not set

--- packages/ui/src/DateTimeField.tsx
+++ packages/ui/src/DateTimeField.tsx
@@ -30,6 +30,9 @@ export const DateTimeField = ({
 
   const formatValue = useCallback(
     (val: string) => {
+      if (!val) {
+        return "";
+      }
       switch (type) {
         case "time":
           return printTime(val, {timezone, showTimezone: true});
