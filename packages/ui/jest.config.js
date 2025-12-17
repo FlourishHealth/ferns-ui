1faeb61e8cd06abb8420457eb191e5b3422788c0
Kyle Kurtz <kyle@kurtz.io>
2025-12-16 21:59:11 -0600

    test fixes

--- packages/ui/jest.config.js
+++ packages/ui/jest.config.js
@@ -1,13 +1,14 @@
 module.exports = {
   preset: "jest-expo",
   transformIgnorePatterns: [
-    "node_modules/(?!(jest-)?react-native|@react-native|react-clone-referenced-element|@react-native-community|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|@sentry/.*)",
+    "node_modules/(?!(jest-)?react-native|@react-native|react-clone-referenced-element|@react-native-community|expo(nent)?|@expo(nent)?/.*|expo-modules-core|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|@sentry/.*)",
   ],
   testEnvironment: "node",
   setupFilesAfterEnv: ["<rootDir>/src/setupTests.ts"],
   moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
   collectCoverageFrom: ["src/**/*.{ts,tsx}", "!src/**/*.d.ts"],
   testMatch: ["**/__tests__/**/*.ts?(x)", "**/?(*.)+(spec|test).ts?(x)"],
+  modulePathIgnorePatterns: ["<rootDir>/dist"],
   transform: {
     "^.+\\.(js|jsx|ts|tsx)$": "babel-jest",
   },
@@ -18,5 +19,8 @@ module.exports = {
   },
   moduleNameMapper: {
     "^@expo/vector-icons$": "<rootDir>/__mocks__/vector-icons.js",
+    "^expo-modules-core/src/Refs$": "<rootDir>/__mocks__/expo-modules-core/src/Refs.ts",
+    "^expo-modules-core/src/web/index.web$":
+      "<rootDir>/__mocks__/expo-modules-core/src/web/index.web.ts",
   },
 };
