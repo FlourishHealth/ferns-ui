cc4d9d920c1538cd10969666f6a538e1b6b42cc8
Josh Gachnang <josh@nang.io>
2025-12-18 22:05:49 -0600

    Show selected emoji

--- apps/demo/stories/EmojiSelector.stories.tsx
+++ apps/demo/stories/EmojiSelector.stories.tsx
@@ -6,7 +6,19 @@ export const EmojiSelectorDemo = (): ReactElement => {
 
   return (
     <Box maxWidth={500} padding={2} gap={2}>
-      <Text>Selected emoji: {selected || "None"}</Text>
+      <Box
+        padding={3}
+        rounding="md"
+        color="lightGray"
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+        direction="row"
+        gap={2}
+      >
+        <Text weight="bold">Selected emoji:</Text>
+        {selected ? <Text size="2xl">{selected}</Text> : <Text color="gray">None</Text>}
+      </Box>
       <Box height={350} overflow="hidden" rounding="md">
         <EmojiSelector
           category={EmojiSelector.defaultProps.category}
